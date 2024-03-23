ee2ae1297183cc4d3dfc82586b7cb6a11458c273
HajwaaN <kevin-hattina@hotmail.com>
2024-03-23 20:52:06 +0100

    Combat interface works as intended. round based combat where the enemy and player takes turns on attacking. whats left is to implement the inventory interaction

--- AdventureGameDemo/AdventureGameDemo/obj/Debug/net8.0/AdventureGameDemo.AssemblyInfo.cs
+++ AdventureGameDemo/AdventureGameDemo/obj/Debug/net8.0/AdventureGameDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventureGameDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a50e2fd0d4f568f3b4353d3e4524f41c0cc6335")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f31b1def8a587d1ffea2e25e4415b133e5df256e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventureGameDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventureGameDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
